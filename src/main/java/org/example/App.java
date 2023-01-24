b9860dd3a75f694ba55c7c3c1cfbf91880dfe4cc
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-01-24 12:18:37 +0700

    Завершена первая аннотация @Component

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,16 +1,17 @@
 package org.example;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-// TODO: Взять необходимые классы из модуля 2
+//  Взять необходимые классы из модуля 2
 //  Классы Logistics и TransportFactory модуля сделать бинами,
 //  TODO: использовать разные способы их конфигурирования.
 public class App 
 {
     public static void main( String[] args )
     {
+        System.out.println("Поднимаю контекст ...");
+
         ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         TransportFactory factory = context.getBean(TransportFactory.class);
 
