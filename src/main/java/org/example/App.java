57df1faf4abecec3768221bcd84c2293e6443b80
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-01-24 12:09:47 +0700

    Аннотировал бинами, поднял контекст. Работает.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 // TODO: Взять необходимые классы из модуля 2
 //  Классы Logistics и TransportFactory модуля сделать бинами,
 //  TODO: использовать разные способы их конфигурирования.
@@ -7,6 +11,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+        TransportFactory factory = context.getBean(TransportFactory.class);
+
+        City city = new City("City",58,true,true);
+        System.out.println(factory.getTransport(city,1000,2).getName());
     }
 }
