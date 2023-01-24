f9d988315e1752a0027c81217bab2c7bcf420eb3
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-01-24 12:36:36 +0700

    Сделал рандомный город (сам не знаю зачем)))

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,8 @@ package org.example;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Random;
+
 //  Взять необходимые классы из модуля 2
 //  Классы Logistics и TransportFactory модуля сделать бинами,
 //  TODO: использовать разные способы их конфигурирования.
@@ -14,8 +16,14 @@ public class App
 
         ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         TransportFactory factory = context.getBean(TransportFactory.class);
-
-        City city = new City("City",58,true,true);
-        System.out.println(factory.getTransport(city,1000,2).getName());
+        
+        System.out.println(factory.getTransport(getRandomCity(),1000,2).getName());
+    }
+    public static City getRandomCity (){
+        Random r = new Random();
+        int distance = r.nextInt(15000);
+        boolean hasAirport = r.nextBoolean();
+        boolean isOnWater = r.nextBoolean();
+        return new City("City of Random",distance,hasAirport,isOnWater);
     }
 }
