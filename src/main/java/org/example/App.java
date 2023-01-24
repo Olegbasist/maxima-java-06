70c5fa3d070132b7b2b07b1739b024743a4015d8
Olegbasist <112417579+Olegbasist@users.noreply.github.com>
2023-01-24 16:33:56 +0700

    Поигрался с рандомным транспортом

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import java.util.Random;
 
+
 //  Взять необходимые классы из модуля 2
 //  Классы Logistics и TransportFactory модуля сделать бинами,
 //  TODO: использовать разные способы их конфигурирования.
@@ -16,14 +17,25 @@ public class App
 
         ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         TransportFactory factory = context.getBean(TransportFactory.class);
-        
-        System.out.println(factory.getTransport(getRandomCity(),1000,2).getName());
+
+        System.out.println("Получаю транспорт:");
+        getTransportFromFactory(factory);
     }
-    public static City getRandomCity (){
-        Random r = new Random();
-        int distance = r.nextInt(15000);
-        boolean hasAirport = r.nextBoolean();
-        boolean isOnWater = r.nextBoolean();
+    private static City getRandomCity (){
+        Random random = new Random();
+        int distance = random.nextInt(15000);
+        boolean hasAirport = random.nextBoolean();
+        boolean isOnWater = random.nextBoolean();
+
         return new City("City of Random",distance,hasAirport,isOnWater);
+    }
+    private static void getTransportFromFactory (TransportFactory factory){
+        Random random = new Random();
+        int weight = random.nextInt(10000);
+        int hours = random.nextInt(24);
+
+        System.out.println(factory.getTransport(getRandomCity(),weight,hours).getName());
+        //return factory.getTransport(getRandomCity(), weight,hours);
+
     }
 }
